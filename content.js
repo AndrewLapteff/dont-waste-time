f3e4c57ae5f4fe8881ec2d76fa0ab6b6c4af0058
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-02-27 08:27:24 +0200

    Add functionality that makes video and tooltip BW

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+const videoStream = document.querySelector('.video-stream.html5-main-video')
+const tooltip = document.querySelector('.ytp-tooltip-bg')
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.text === 'display_alert') {
     let button = document.querySelector('.ytp-play-button.ytp-button')
@@ -8,3 +11,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     chrome.runtime.sendMessage({ text: 'alert_closed' })
   }
 })
+
+if (videoStream) {
+  videoStream.style.filter = 'grayscale(100%)'
+}
+if (tooltip) {
+  tooltip.style.filter = 'grayscale(100%)'
+}
